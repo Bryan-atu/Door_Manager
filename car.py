583d2f68142ca7486f4a8824fc94f1b118168586
Bryan-atu <L001885441@atu.ie>
2024-11-15 21:49:11 +0000

    [feat/unit_test1] add unit tests

--- car.py
+++ car.py
@@ -1,6 +1,13 @@
+import sys
+import os
+
+# Add the parent directory (Door_Manager) to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
 class Car:
-    def__init0(self, num_doors=4):
-        self.num_doors=num_doors
+    def __init__(self, num_doors=4):
+        self.num_doors = num_doors
 
     def get_num_doors(self):
         return self.num_doors
@@ -9,4 +16,4 @@ class Car:
         if num_doors > 0:
             self.num_doors = num_doors
         else:
-            raise ValueError("number of doors must be positive int")
+            raise ValueError("number of doors must be a positive integer")
